046b5321141496ed5f872c01c7fcc27778774d67
dotnotation <dotinmotionproductions@gmail.com>
2022-03-01 17:54:00 -0500

    added seed data and checked that everything is associated correctly in console

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+momo = Pet.create(
+    name: "Momo",
+    photo: "https://cf.ltkcdn.net/dogs/images/orig/127015-686x700-Crested-pup-1.jpg"
+)
+
+rootbeer = Pet.create(
+    name: "Rootbeer",
+    photo: "https://i.pinimg.com/236x/f0/7d/ed/f07ded93ef5f6e2d287a2606562dc45f.jpg"
+)
+
+rootbeer.toys.build(
+    name: "Skinny Plush Squeaking Unicorn",
+    photo: "https://img.chewy.com/is/image/catalog/174713_Main._AC_SL1500_V1572616704_.jpg",
+    price: "$5.98",
+    brand: "Frisco",
+    url: "https://www.chewy.com/frisco-skinny-plush-squeaking-unicorn/dp/201402",
+    rating: 4,
+    needs_repair: false,
+    squeaker: true,
+    crinkle: false,
+    treat: false,
+    pet_id: 2
+)
+
+momo.toys.build(
+    name: "Forest Friends Stuffing-Free Skinny Plush",
+    photo: "https://img.chewy.com/is/image/catalog/286571_MAIN._AC_SL1500_V1634235982_.jpg",
+    price: "$9.98",
+    brand: "Frisco",
+    url: "https://www.chewy.com/frisco-forest-friends-stuffing-free/dp/312936",
+    rating: 5,
+    needs_repair: false,
+    squeaker: true,
+    crinkle: true,
+    treat: false,
+    pet_id: 1
+)
+
+momo.save 
+rootbeer.save 
